090d266e67a622c589fffcde6a6b21353dcb3829
Alex <52381045+AlexandroLuis@users.noreply.github.com>
2021-03-10 12:53:06 -0300

    Update ICMP.sql

--- Projeto/ICMP.sql
+++ Projeto/ICMP.sql
@@ -14,18 +14,46 @@ use ICMP;
 create table if not exists Leito(
 	idLeito int(11) primary key,
 	Status bool not null
+	/* Incluir FK'S */
+	/*Possíveis atribuições: função do leito, equipamentos, chefe de departamento, */
+);
+
+create table if not exists Endereco(
+	idEndereco integer(11) primary key,
+	Rua varchar(255) not null,
+	numero varchar(20) not null,
+	complemento varchar(255),
 );
 
 /* 
 	Informações do paciente que será tratado na unidade 
 	
 */
+create table if not exists ProblemasdoPaciente(
+	/* 
+		Incluir todas as observações de problemas do paciente para futuras consultas
+	*/
+
+
+);
+
 create table if not exists Paciente(
 	idPaciente integer(11) primary key, /* id do paciente deve ser id padrão, pois o mesmo pode não ter outra forma de identificação */
 	CartaoSus integer(tamanho), 
 	Nome varchar(255) not null,
 	Cpf integer(11) not null,
 	DataNascimento date not null
+	/* Incluir endereço e FK'S */
+);
+
+/* Tabela para listar os atendimentos realizados com os clientes 1-N atendimentos */
+create table if not exists Atendimentos(
+	/* 
+		Incluir FK'S
+		data de entrada, saida, problemas, atendimento realizado, status, problemas futuros...
+	*/
+
+
 );
 
 /* 
@@ -37,6 +65,7 @@ create table if not exists Funcionario(
 	Rg integer(13) not null,
 	Cpf integer(11) not null,
 	DataNascimento date not null
+	/* Incluir endereço e FK'S */
 );
 
 /* 
@@ -46,4 +75,5 @@ create table if not exists Funcionario(
 create table if not exists OcupacaoLeito(
 	idLeito integer(11) primary key,
 	NomePaciente varchar(255)
+	/* incluir FK'S */
 );
